e0b1a4c08f01036dac9743980c3ebd3dd63cde14
olivercrush <olivier.beguinti@gmail.com>
2021-03-21 21:43:00 -0400

    added base stats, deleted armors

--- src/Battle/Entity/CharacteristicsContainer.cs
+++ src/Battle/Entity/CharacteristicsContainer.cs
@@ -8,29 +8,30 @@ namespace ShipCore.Battle.Entity
 {
     public class CharacteristicsContainer
     {
+        private Characteristics _base;
         private int _health;
-        private int _pArmor;
-        private int _mArmor;
         private int _mobility;
         private int _initiative;
 
         public CharacteristicsContainer(Characteristics characteristics)
         {
+            _base = characteristics;
             _health = characteristics.Health;
-            _pArmor = characteristics.PArmor;
-            _mArmor = characteristics.MArmor;
             _mobility = characteristics.Mobility;
             _initiative = characteristics.Initiative;
         }
 
         public Characteristics GetCharacteristics()
         {
-            return new Characteristics(_health, _pArmor, _mArmor, _mobility, _initiative);
+            return new Characteristics(_health, _mobility, _initiative);
+        }
+
+        public Characteristics GetBaseCharacteristics()
+        {
+            return _base;
         }
 
         public void UpdateHealth(UpdateMethod updateMethod, int value) { _health = (updateMethod == UpdateMethod.SET) ? value : _health + value ; }
-        public void UpdatePArmor(UpdateMethod updateMethod, int value) { _pArmor = (updateMethod == UpdateMethod.SET) ? value : _pArmor + value ; }
-        public void UpdateMArmor(UpdateMethod updateMethod, int value) { _mArmor = (updateMethod == UpdateMethod.SET) ? value : _mArmor + value ; }
         public void UpdateMobility(UpdateMethod updateMethod, int value) { _mobility = (updateMethod == UpdateMethod.SET) ? value : _mobility + value ; }
         public void UpdateInitiative(UpdateMethod updateMethod, int value) { _initiative = (updateMethod == UpdateMethod.SET) ? value : _initiative + value ; }
     }
@@ -44,23 +45,19 @@ namespace ShipCore.Battle.Entity
     public readonly struct Characteristics
     {
         public int Health { get; }
-        public int PArmor { get; }
-        public int MArmor { get; }
         public int Mobility { get; }
         public int Initiative { get; }
 
-        public Characteristics(int health, int pArmor, int mArmor, int mobility, int initiative)
+        public Characteristics(int health, int mobility, int initiative)
         {
             Health = health;
-            PArmor = pArmor;
-            MArmor = mArmor;
             Mobility = mobility;
             Initiative = initiative;
         }
 
         public override String ToString()
         {
-            return ("health : " + Health + ", pArmor : " + PArmor + ", mArmor : " + MArmor + ", mobility : " + Mobility + ", initiative : " + Initiative);
+            return ("health : " + Health + ", mobility : " + Mobility + ", initiative : " + Initiative);
         }
     }
 }
